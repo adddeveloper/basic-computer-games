cf8318d261d2de1940f1da4d06619c084ca6097b
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-10-15 21:29:51 -0300

    update

--- snake-game/index.js
+++
@@ -1,9 +0,0 @@
-// import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
-kaboom({
-    global: true,
-    fullscreen: true,
-    debug: true,
-    scale: 1,
-    background: [255, 255, 255],
-    canvas: document.getElementById("canvas"),
-})
